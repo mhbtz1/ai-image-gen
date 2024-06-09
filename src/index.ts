5166c6464d0e110eaef66f305e4e6c82ef2ee04b
Matthew Habtezgi <mhbtz@mit.edu>
2024-06-09 13:17:41 -0400

    set up cloudflare workers :)

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import { Hono } from 'hono'
+import { serve } from '@hono/node-server'
 
-type Bindings = {
-  [key in keyof CloudflareBindings]: CloudflareBindings[key]
-}
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/', async (c) => {
+    // Picture of a dog
+    const inputs = {
+      prompt: "cyberpunk cat",
+    };
+
+    const response = await c.env.AI.run(
+      "@cf/bytedance/stable-diffusion-xl-lightning",
+      inputs
+    );
+
+    return new Response(response, {
+      headers: {
+        "content-type": "image/png",
+      },
+    });
+
 })
 
 export default app
